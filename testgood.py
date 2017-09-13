60b4257010151b3d84138993eb49591e7b811bf6
hao-top <775966553@qq.com>
2017-09-13 11:02:52 +0800

    2017年9月13日09:09:44

--- testgood.py
+++ testgood.py
@@ -2,5 +2,13 @@
 
 # 最好自己
 result=["one","two",'three',['1',2,3,4],2,3,4,5]
-list.append(result,6)
-print(result)
+# list.append(result,6)
+# result.pop()
+# print(result)
+def eat(testt):
+    for i in range(0,8):
+        testt.pop(0)
+        i-=1
+        print(testt)
+
+eat(result)
